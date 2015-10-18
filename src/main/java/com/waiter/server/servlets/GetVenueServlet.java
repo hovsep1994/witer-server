eb7b3e62dc6a15ea751dae0f58137c27a3dd562e
hovseo <hovsep1994@gmail.com>
2015-10-18 11:44:53 +0400

    testings

---
+++ src/main/java/com/waiter/server/servlets/GetVenueServlet.java
@@ -0,0 +1,28 @@
+package com.waiter.server.servlets;
+
+import com.waiter.server.commons.entities.Venue;
+import com.waiter.server.response.IResponseWriter;
+import com.waiter.server.response.JsonResponseWriter;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Admin on 10/18/2015.
+ */
+public class GetVenueServlet extends BaseServlet {
+    private static final Logger LOG = Logger.getLogger(RegisterVenueServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        IResponseWriter<Venue> writer = new JsonResponseWriter<>(resp.getWriter());
+        writer.writeResponse(new Venue().setName("resp"));
+
+
+    }
+}
