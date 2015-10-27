a4ac0d48c0867ac2a1951c1e18d4624549f9eeaf
shahenpoghosyan <shahen.poghosyan@picsart.com>
2015-10-27 15:29:58 +0400

    added map and pin on it when selecting address

--- web/index.jsp
+++ web/index.jsp
@@ -12,39 +12,60 @@
 <html>
 <head>
     <title>Home</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="./scripts/reg_venue.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="./styles/reg_venus.css">
+    <style>
+        #map {
+            height: 400px;
+            width: 400px;
+        }
+    </style>
+    <script type="text/javascript">
+
+
+    </script>
 </head>
 <body>
 <%
     ApplicationContext context = (ApplicationContext) pageContext.getServletContext().getAttribute("springContext");
     LocationsJDBCTemplate locationJDBCTemplate = (LocationsJDBCTemplate) context.getBean("locationJDBCTemplate");
 %>
-<form name="venue" >
+<form name="venue">
 
     <label>Name: </label>
-    <input class="reg_input" name="venueName" type="text" /> <br>
+    <input class="reg_input" name="venueName" type="text"/> <br>
     <label>Email: </label>
-    <input class="reg_input" name="ownerEmail" type="email" /> <br>
+    <input class="reg_input" name="ownerEmail" type="email"/> <br>
     <label>Password: </label>
-    <input class="reg_input" name="password" type="password" /> <br>
+    <input class="reg_input" name="password" type="password"/> <br>
     <label>Repeat password: </label>
     <input class="reg_input" name="repeatPassword" type="password"> <br>
     <label>Country: </label>
-    <select id="country" class="reg_input" name = "venueCountry" >
+    <select id="country" class="reg_input" name="venueCountry">
         <option value="" disabled selected>Select Country</option>
         <% for (Country country : locationJDBCTemplate.getAllCountries()) {
             System.out.println(country); %>
-        <option value="<%=country.getCode()%>" ><%=country.getName()%></option>
+        <option value="<%=country.getCode()%>"><%=country.getName()%>
+        </option>
         <% } %>
     </select> <br>
     <label>City: </label>
-    <input id="city" class="reg_input" list="cities_data" name="city" />
-    <datalist id="cities_data" >
-        <option>Armenia</option>
-    </datalist> <br>
+    <input id="city" class="reg_input" list="cities_data" name="city"/>
+    <datalist id="cities_data">
+    </datalist>
+    <br>
+    <label>Address: </label>
+    <input id="address" class="reg_input" name="address"/> <br>
     <input class="reg_input" name="venueSubmit" type="submit">
+
+    <div id="map">
+    </div>
+
 </form>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="./scripts/reg_venue.js" type="text/javascript"></script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAW_hI04roTnEO08eq4tlKgNh4okOdMSTE&callback=initMap">
+</script>
 </body>
 </html>
--- web/scripts/reg_venue.js
+++ web/scripts/reg_venue.js
@@ -1,6 +1,18 @@
 /**
  * Created by shahenpoghosyan on 10/24/15.
  */
+
+var map;
+var geocoder;
+var marker;
+function initMap() {
+    geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 1
+    });
+}
+
 $(document).ready(function () {
     $('#city').on("input", function (e) {
         function searchCities(countryCode, query) {
@@ -9,7 +21,7 @@ $(document).ready(function () {
                 query: query,
                 limit: 10
             }, function (data) {
-                if ($('#country option:selected').val()) {
+                if ($('#country').find('option:selected').val()) {
                     var response = JSON.parse(data);
                     if (response.success) {
                         var html = '';
@@ -21,8 +33,59 @@ $(document).ready(function () {
                 }
             })
         }
-            searchCities($('#country option:selected').val(),
-                $('#city').val() );
+        if($('#city').val().length == 1) {
+            searchCities($('#country').find('option:selected').val(), $('#city').val());
+        }
+
     });
 
+    $('#country').focusout(function () {
+        codeAddress($('#country').find('option:selected').text(),
+            $('#city').val(), $('#address').val());
+    });
+
+    $('#city').focusout(function () {
+        codeAddress($('#country').find('option:selected').text(),
+            $('#city').val(), $('#address').val());
+    });
+
+    $('#address').focusout(function () {
+        codeAddress($('#country').find('option:selected').text(),
+            $('#city').val(), $('#address').val());
+    });
+
+    function codeAddress(country, city, address) {
+        if (!country) return;
+        var combined = country;
+        map.setZoom(6);
+        if (city) {
+            combined = city + ", " + combined;
+            map.setZoom(10);
+        }
+        if (address) {
+            combined = address + ", " + combined;
+            map.setZoom(14);
+        }
+
+        console.log(combined);
+        geocoder.geocode({'address': combined}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                if(!marker) {
+                    marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location,
+                        draggable: true
+                    });
+                }
+                marker.setPosition(results[0].geometry.location);
+            } else {
+                //todo remove alert
+                alert("Geocode was not successful for the following reason: " + status);
+                if(marker) {
+                    marker.setMap(null);
+                }
+            }
+        });
+    }
 });
