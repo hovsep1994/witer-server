11c2baf812a40299db1581bef996bf26e8f8402f
shahenpoghosyan <shahen.poghosyan@picsart.com>
2017-03-18 19:28:20 +0400

    property file name changed.

--- src/main/java/com/waiter/server/config/RootConfig.java
+++ src/main/java/com/waiter/server/config/RootConfig.java
@@ -43,7 +43,7 @@ public class RootConfig {
         Resource[] resources = new Resource[3];
         resources[0] = new ClassPathResource("menukit.properties");
         resources[1] = new ClassPathResource("development.properties");
-        resources[2] = new FileSystemResource("file:${user.home}/couchcoach.properties");
+        resources[2] = new FileSystemResource("file:${user.home}/menukit.properties");
         propertiesFactoryBean.setLocations(resources);
         propertiesFactoryBean.setIgnoreResourceNotFound(true);
         propertiesFactoryBean.afterPropertiesSet();
