a6b5bec2033187503ea53eb0f7056e48a9b13ce3
hovsep94 <hovsep.harutyunyan@sflpro.com>
2017-04-20 22:55:31 +0400

    Add offset and limit

--- src/main/java/com/waiter/server/api/venue/VenueClientController.java
+++ src/main/java/com/waiter/server/api/venue/VenueClientController.java
@@ -132,14 +132,16 @@ public class VenueClientController extends MainController {
         searchParameters.setLatitude(latitude);
         searchParameters.setLongitude(longitude);
         searchParameters.setSort(VenueSearchParameters.Sort.GEODIST);
-        searchParameters.setDistance((double) 100000);
+        searchParameters.setLimit(20);
+        searchParameters.setOffset(0);
+        searchParameters.setDistance(1.0);
         final List<Venue> venues = venueSearchService.getVenuesBySearchParameters(searchParameters);
         LOGGER.info(venues.toString());
         final List<VenueClientNearbyModel> modelList = venues.stream()
                 .map(venue -> VenueClientNearbyModel.convert(venue, language, null))
                 .collect(Collectors.toList());
 
-        return MenuKitResponseEntity.success(venues);
+        return MenuKitResponseEntity.success(modelList);
     }
 
 
